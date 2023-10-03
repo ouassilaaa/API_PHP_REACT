8e3abc5e9fec19bfa18113c3c1bdb82e50373170
ouassilaaa <ouassidu31@hotmail.fr>
2023-10-03 20:56:07 +0200

    commentaires

--- index.php
+++ index.php
@@ -1,18 +1,16 @@
 <?php
-
-
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: *");
+
 include './utils/bddConnect.php';
 
 
-
+//nouvel object de connexion à la BDD
 $connexion = new BddConnect();
 $bdd = $connexion->connexion();
 
-
-
+//autoriser l'accès page
 $method = $_SERVER['REQUEST_METHOD'];
 switch ($method) {
 
@@ -38,8 +36,6 @@ switch ($method) {
         $sql = "INSERT INTO users (name,surname, mobile,email,note) VALUES('$name','$surname','$mobile','$email', '$note');";
         $req = $bdd->prepare($sql);
 
-        //nettoyage nom via function
-        $name = Fonction::cleandata($_POST['name']);
 
         // affectation des variables
         $req->bindParam(1, $user->name, PDO::PARAM_STR);
@@ -51,40 +47,53 @@ switch ($method) {
         //exécution de la requête
         if ($req->execute()) {
             $response = ['status' => 1, 'message' => 'Record created successfully.'];
-            // header('http: ')
         } else {
             $response = ['status' => 0, 'message' => 'Failed to create record.'];
         }
         echo json_encode($response);
+
+        //nettoyage des inputs
+        function cleanInput($input)
+        {
+            return htmlspecialchars(strip_tags(trim($input)));
+        }
         break;
 
-    case "PUT":
+    case "UPDATE":
         $user = json_decode(file_get_contents('php://input'));
         $sql = "UPDATE users SET name= :name, email =:email, mobile =:mobile, updated_at =:updated_at WHERE id = :id";
-        $stmt = $conn->prepare($sql);
+        $req = $bdd->prepare($sql);
         $updated_at = date('Y-m-d');
-        $stmt->bindParam(':id', $user->id);
-        $stmt->bindParam(':name', $user->name);
-        $stmt->bindParam(':email', $user->email);
-        $stmt->bindParam(':mobile', $user->mobile);
-        $stmt->bindParam(':updated_at', $updated_at);
 
-        if ($stmt->execute()) {
+        // affectation des variables
+        $req->bindParam(1, $user->name, PDO::PARAM_STR);
+        $req->bindParam(2, $user->surname, PDO::PARAM_STR);
+        $req->bindParam(3, $user->email, PDO::PARAM_STR);
+        $req->bindParam(4, $user->mobile, PDO::PARAM_STR);
+        $req->bindParam(5, $user->note, PDO::PARAM_STR);
+
+        //exécution de la requête
+        if ($req->execute()) {
             $response = ['status' => 1, 'message' => 'Record updated successfully.'];
         } else {
             $response = ['status' => 0, 'message' => 'Failed to update record.'];
         }
         echo json_encode($response);
+
+        function cleanInput($input)
+        {
+            return htmlspecialchars(strip_tags(trim($input)));
+        }
         break;
 
     case "DELETE":
         $sql = "DELETE FROM users WHERE id = :id";
         $path = explode('/', $_SERVER['REQUEST_URI']);
 
-        $stmt = $conn->prepare($sql);
-        $stmt->bindParam(':id', $path[3]);
+        $req = $bdd->prepare($sql);
+        $req->bindParam(':id', $path[3]);
 
-        if ($stmt->execute()) {
+        if ($req->execute()) {
             $response = ['status' => 1, 'message' => 'Record deleted successfully.'];
         } else {
             $response = ['status' => 0, 'message' => 'Failed to delete record.'];
